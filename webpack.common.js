86606b99c91a4871b14f519c3acb084670b3efe4
nvando <vandobkdn@gmail.com>
2021-01-11 01:06:03 +0700

    webpack env config

--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'public'),
-        filename: '[name].bundle.js',
+        filename: '[name].[contenthash].bundle.js'
     },
     devServer: {
         port: 3000,
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,15 @@
 const { merge } = require('webpack-merge');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const path = require('path');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'development',
-    devtool: 'source-map',
+    devtool: 'inline-source-map',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].bundle.js',
+    },
     devServer: {
         port: 4200,
         proxy: {
